f506e0f84ffbee62903902a5b4d79c8aea78894b
ChiChaChai <chatchaishaetan@gmail.com>
2018-12-22 12:22:59 +0700

    Add simple echo bot

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const line = require('@line/bot-sdk');
+const config = require('./config');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const lineClient = new line.Client({
+    channelSecret: config.lineChannelSecret,
+    channelAccessToken: config.lineChannelAccessToken
+});
+const app = express();
+app.use(cors({
+    origin: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/', line.middleware({
+    channelSecret: config.lineChannelSecret,
+    channelAccessToken: config.lineChannelAccessToken
+}), (req, res) => {
+    const events = req.body.events;
+    // console.log(events);
+    Promise.all(events.map((event) => {
+        console.log(event);
+        return lineClient.replyMessage(event.replyToken, event.message);
+    })).then(() => {
+        res.json({});
+    });
+
+});
+
+
+exports.helloWorld = functions.https.onRequest(app);
